5bc3587b164d67d239adcc36b3f328cfe39e89f8
2minzy <minjilee0423@gmail.com>
2020-08-17 00:14:33 +0900

    Updated gitignore

--- index.js
+++ index.js
@@ -1,21 +1,50 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const app = express();
+const port = 5000;
+const bodyParser = require('body-parser');
+const config = require('./config/key');
+const { User } = require('./models/User');
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://Minji:123abc@boiler-plate.vjl9k.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+// application/x--www-form-urlencoded 을 분석해서 가져오기 위해서
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+// application/json 을 분석해서 가져오기 위해서
+app.use(bodyParser.json());
+
+const mongoose = require('mongoose');
+mongoose
+  .connect(config.mongoURI, {
     // to avoid error
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
-    useFindAndModify: false
-  }).then(() => console.log('MonjoDB Connected...'))
-  .catch(err => console.log(err))
+    useFindAndModify: false,
+  })
+  .then(() => console.log('MonjoDB Connected...'))
+  .catch((err) => console.log(err));
 
 app.get('/', (req, res) => {
-  res.send('Hello World! 안녕하세요')
-})
+  res.send('Hello World! 안녕하세요');
+});
+
+app.post('/register', (req, res) => {
+  // 회원 가입 할 때 필요한 정보들을 client에서 가져오면
+  // 그것들을 DB에 넣어준다.
+
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
